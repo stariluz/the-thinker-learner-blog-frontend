4822c3166b9a14ff103dd35c29e4cade832f62c8
Adrigondo <adrigondo.profesional@gmail.com>
2022-09-07 10:51:13 -0600

    Add: Creating Components and adding bootstrap. 4 files modified, 11 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
+  title = 'The Thinker Learner Blog';
   name = 'Angular ' + VERSION.major;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MainComponent } from './layout/main/main.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, MainComponent, HeaderComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
