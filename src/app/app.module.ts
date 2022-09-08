f478fb12c7ed23cef67ae922ad3ec494b37145ac
Adrigondo <adrigondo.profesional@gmail.com>
2022-09-08 11:51:35 -0600

    Update: temporal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { MainComponent } from './layout/main/main.component';
+import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, MainComponent, HeaderComponent],
+  declarations: [AppComponent, HomeComponent, MainComponent, HeaderComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
