bdfa41c7872cc0994acb7f653c071a5de50ba03c
Adrigondo <adrigondo.profesional@gmail.com>
2022-09-09 11:56:07 -0600

    Add: Adding first styles and icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = 'The Thinker Learner Blog';
