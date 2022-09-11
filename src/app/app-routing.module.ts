b91fc402f0d94bc3187f47e73316c70a071017e6
Adrigondo <adrigondo.profesional@gmail.com>
2022-09-11 13:53:12 -0600

    Add: News detail view and navigation routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './layout/main/main.component';
 import { HomeComponent } from './components/home/home.component';
 import { NewsCreateComponent } from './components/news/news-create/news-create.component';
 import { NewsDetailComponent } from './components/news/news-detail/news-detail.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'news', pathMatch: 'full' },
   {
-    path: 'news',
+    path: '',
     component: AppComponent,
     children: [
-      { path: ':id', component: NewsDetailComponent },
-      { path: 'create', component: NewsCreateComponent },
+      { path: '', redirectTo: 'news', pathMatch: 'full' },
+      {
+        path: 'news',
+        component: MainComponent,
+        children: [
+          { path: '', component: HomeComponent },
+          { path: 'create', component: NewsCreateComponent },
+          { path: ':id', component: NewsDetailComponent },
+        ],
+      },
     ],
   },
 ];
