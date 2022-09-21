8ad60b6fdee9ca20f38dc12498ab5aba25f13131
Adrian González <adrigondo.profesional@gmail.com>
2022-09-20 19:19:22 -0600

    Revert "Update: markdown editor"
    This reverts commit 6dde6634479c5d5010e92038017b67cbcba80d08.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { NgxDropzoneModule } from 'ngx-dropzone';
 import { MarkdownModule } from 'ngx-markdown';
-import { SimplemdeModule } from 'ngx-simplemde';
+import { LMarkdownEditorModule } from 'ngx-markdown-editor';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './layout/main/main.component';
@@ -29,7 +29,7 @@ import { ImageInputComponent } from './components/molecules/image-input/image-in
     FormsModule,
     NgxDropzoneModule,
     MarkdownModule.forRoot({ loader: HttpClient }),
-    SimplemdeModule.forRoot(),
+    LMarkdownEditorModule,
   ],
   declarations: [
     AppComponent,
